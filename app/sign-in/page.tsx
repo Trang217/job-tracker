2b6fdd9978445410cf6ac49cfab541f3f85c2f85
Trang <nguyenminhtrang9213@gmail.com>
2026-03-25 16:42:08 +0100

    add authenticate and sign in, sign up page

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,3 +1,78 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import Link from "next/link";
+
 export default function SignIn() {
-  return <div>sign in</div>;
+  return (
+    <div className="flex min-h-[calc(100vh-4rem)] items-center justify-center bg-amber-50">
+      <Card className="w-full max-w-md border-gray-200 shadow-lg">
+        <CardHeader className="space-y-1">
+          <CardTitle className="text-2xl font-bold text-black">
+            Sign In
+          </CardTitle>
+          <CardDescription className="text-gray-600">
+            Enter your credential to access your account
+          </CardDescription>
+        </CardHeader>
+
+        <form className="space-y-4">
+          <CardContent className="space-y-4">
+            <div className="space-y-2">
+              <Label htmlFor="email" className="text-gray-700">
+                Email
+              </Label>
+              <Input
+                id="email"
+                type="email"
+                placeholder="jenny@example.com"
+                required
+                className=" focus:border-rose-400 focus:ring-rose-400"
+              />
+            </div>
+
+            <div className="space-y-2">
+              <Label htmlFor="password" className="text-gray-700">
+                Password
+              </Label>
+              <Input
+                id="password"
+                type="password"
+                minLength={8}
+                required
+                className="border-gray-300 focus:border-rose-400 focus:ring-rose-400"
+              />
+            </div>
+          </CardContent>
+          <CardFooter className="flex flex-col space-y-4">
+            <Button
+              type="submit"
+              className="w-full font-bold bg-rose-400 hover:bg-rose-400/90"
+            >
+              Sign In
+            </Button>
+            <p className="text-center text-sm text-gray-600">
+              Don't have an account?{" "}
+              <Link
+                className="font-bold text-rose-400 hover:underline"
+                href="/sign-up"
+              >
+                Sign Up
+              </Link>
+            </p>
+          </CardFooter>
+        </form>
+      </Card>
+    </div>
+  );
 }
